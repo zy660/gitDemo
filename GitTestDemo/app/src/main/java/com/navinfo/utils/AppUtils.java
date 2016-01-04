fea5925150215e66703f4daff1cf00ad515f9038
ezyong <zhaoyong4435@navinfo.com>
2016-01-04 14:47:39 +0800

    add appUtils

---
+++ GitTestDemo/app/src/main/java/com/navinfo/utils/AppUtils.java
@@ -0,0 +1,8 @@
+package com.navinfo.utils;
+
+public class AppUtils {
+
+    public static final String activity_result0 = "0";
+    public static final String activity_result2 = "2";
+    public static final String activity_result4 = "4";
+}
