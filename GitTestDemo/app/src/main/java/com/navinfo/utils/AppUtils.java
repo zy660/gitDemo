79a2e7e0e31302e26b862b96fec118bb3fe0a2ae
ezyong <zhaoyong4435@navinfo.com>
2016-01-04 15:19:09 +0800

    add empty

--- GitTestDemo/app/src/main/java/com/navinfo/utils/AppUtils.java
+++ GitTestDemo/app/src/main/java/com/navinfo/utils/AppUtils.java
@@ -3,6 +3,16 @@ package com.navinfo.utils;
 public class AppUtils {
 
     public static final String activity_result0 = "0";
+    public static final String activity_result1 = "1";
     public static final String activity_result2 = "2";
+    public static final String activity_result3 = "3";
     public static final String activity_result4 = "4";
+
+    public static boolean isEmpty(String txt){
+        boolean result = false;
+        if (txt != null || !"".equals(txt)){
+            result = true;
+        }
+        return result;
+    }
 }
